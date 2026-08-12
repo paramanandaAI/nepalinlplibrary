7603a0d6ce7ec09d12cf74f3788bb41fffac3510
aitalimcom <aitalim.com@gmail.com>
2026-08-12 16:45:29 +0545

    start only

---
+++ llm_prompts/main.py
@@ -0,0 +1,10 @@
+make folders and such domain and problem wise for now its now a placeholder 
+
+
+
+
+
+- this will have t5 whisper gemma4 and such support we will use langchain as our tool during api and mcp time 
+
+
+- huggingface and such dependency will be there okay 
